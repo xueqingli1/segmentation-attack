575bdd6c77bafa0052e45249c152a5964ac3f432
Xueqing Li <xueqingli@lawn-128-61-62-83.lawn.gatech.edu>
2022-11-22 00:13:15 -0500

    image_epsilon

--- attack_image_epsilon.py
+++ attack_image_epsilon.py
@@ -7,35 +7,35 @@ from utils import visualize_attack_results
 import matplotlib.pyplot as plt
 
 
-# class EpsilonImageAttack:
-#     def __init__(self, model):
-#         self.model = model
-#         self.epsilon = torch.zeros((256, 256))
-#
-#     def epsilon_attack(self, x, y, alpha=0.01, max_iter=300, clipping=0.1):
-#         self.model.eval()
-#         x_fooling = x.clone()
-#         y_target = torch.where(y == 1, 2, y)
-#
-#         ones = (y_target == 1).sum()
-#         print("target label", ones)
-#
-#         epsilon = torch.zeros_like(x)
-#         x_fooling_var = Variable(x_fooling, requires_grad=True)
-#
-#         for it in range(max_iter):
-#             attack_image = x_fooling_var + epsilon
-#             score_model = self.model(attack_image)
-#             loss = self.model.loss(score_model, y_target)
-#             loss.backward()
-#
-#             gradient = x_fooling_var.grad.data
-#             epsilon -= torch.sign(gradient) * alpha
-#             epsilon = torch.clamp(epsilon, min=-clipping, max=clipping)
-#             x_fooling_var.grad.fill_(0)
-#
-#         self.epsilon = epsilon
-#         return epsilon
+class EpsilonImageAttack:
+    def __init__(self, model):
+        self.model = model
+        self.epsilon = torch.zeros((256, 256))
+
+    def epsilon_attack(self, x, y, alpha=0.01, max_iter=100, clipping=0.1):
+        self.model.eval()
+        x_fooling = x.clone()
+        y_target = torch.where(y == 1, 2, y)
+
+        ones = (y_target == 1).sum()
+        print("target label", ones)
+
+        epsilon = torch.zeros_like(x)
+        x_fooling_var = Variable(x_fooling, requires_grad=True)
+
+        for it in range(max_iter):
+            attack_image = x_fooling_var + epsilon
+            score_model = self.model(attack_image)
+            loss = self.model.loss(score_model, y_target)
+            loss.backward()
+
+            gradient = x_fooling_var.grad.data
+            epsilon -= torch.sign(gradient) * alpha
+            epsilon = torch.clamp(epsilon, min=-clipping, max=clipping)
+            x_fooling_var.grad.fill_(0)
+
+        self.epsilon = epsilon
+        return epsilon
 
 
 def epsilon_attack(model, x, y):
@@ -71,7 +71,7 @@ def run_attack():
     model = SegmentationModule.load_from_checkpoint(checkpoint_path)
     model.eval()
     dataloader = NeuroDataModule(32).test_dataloader()
-    # attack = Attack()
+    attack = EpsilonImageAttack(model)
 
     for idx, batch in enumerate(dataloader):
         if idx == 0:
@@ -80,9 +80,9 @@ def run_attack():
             y = y[0].unsqueeze(0)
             y_pred = model.predict(x)
 
-            # X_fooling, perturbation = attack.make_perturbation(x, y, 0, model)
+            perturbation = attack.epsilon_attack(x, y)
 
-            perturbation = epsilon_attack(model, x, y)
+            # perturbation = epsilon_attack(model, x, y)
             X_fooling = x + perturbation
 
             y_attack_pred = model.predict(X_fooling)
