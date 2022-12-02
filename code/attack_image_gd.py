e3af818ea7bbe23d555142d355a0b8c34e363ac3
Xueqing Li <xueqingli@lawn-128-61-48-51.lawn.gatech.edu>
2022-12-02 14:00:26 -0500

    add clipping to image gd

--- code/attack_image_gd.py
+++ code/attack_image_gd.py
@@ -10,28 +10,29 @@ from labels2 import hide_with_nearest_segment
 
 
 class GradientDescentImageAttack:
-    def make_perturbation(self, model, x, y_target, learning_rate=0.01, max_iter=500):
+    def make_perturbation(self, model, x, y_target, learning_rate=0.5, max_iter=100, clipping=0.2):
         """
         Generates a perturbation
         """
         model.eval()
         X_fooling = x.clone()
         X_fooling_var = Variable(X_fooling, requires_grad=True)
+        perturbation = torch.zeros_like(x)
 
         for it in range(max_iter):
-            score_model = model(X_fooling_var)
+            attack_image = X_fooling_var + perturbation
+            attack_image = torch.clamp(attack_image, min=0, max=1)
+            score_model = model(attack_image)
             loss = model.loss(score_model, y_target)
             loss.backward()
 
             gradient = X_fooling_var.grad.data
             norm = torch.sqrt(torch.sum(gradient ** 2))
-            X_fooling_var.data -= learning_rate * gradient / norm
+            perturbation -= learning_rate * gradient / norm
+            perturbation = torch.clamp(perturbation, min=-clipping, max=clipping)
             X_fooling_var.grad.fill_(0)
-            X_fooling_var.data = torch.clamp(X_fooling_var.data, min=0, max=1)
 
-        X_fooling = X_fooling_var.data
-        perturbation = X_fooling - x
-        return X_fooling, perturbation
+        return perturbation
 
 
 def run_attack():
@@ -50,42 +51,16 @@ def run_attack():
 
             for hide_label in range(4):
                 y_target = hide_with_nearest_segment(y, hide_label)
-                X_fooling, perturbation = attack.make_perturbation(model, x, y_target, learning_rate=0.01, max_iter=500)
+                perturbation = attack.make_perturbation(model, x, y_target, learning_rate=0.5, max_iter=100)
+                X_fooling = x + perturbation
+                X_fooling = torch.clamp(X_fooling, min=0, max=1)
                 y_attack_pred = model.predict(X_fooling)
                 print("hide label", hide_label)
                 print("before attack", count_label(y_pred))
                 print("after attack", count_label(y_attack_pred))
 
-                visualize_attack_results(f"nn_tst_hide_{hide_label}_image_gd",
+                visualize_attack_results(f"nn_clip_{hide_label}_image_gd",
                                          x, X_fooling, perturbation, y, y_target, y_attack_pred)
-
-            # for hide_label in range(4):
-            #     for target_label in range(4):
-            #         if target_label != hide_label:
-            #             y_target = generate_hide_one_label(y, hide_label, target_label)
-            #             X_fooling, perturbation = attack.make_perturbation(model, x, y_target, learning_rate=0.01, max_iter=500)
-            #             y_attack_pred = model.predict(X_fooling)
-            #             print("hide label", hide_label, "target label", target_label)
-            #             print("before attack", count_label(y_pred))
-            #             print("after attack", count_label(y_attack_pred))
-            #
-            #             visualize_attack_results(f"hide_{hide_label}_target_{target_label}_image_gd",
-            #                                      x, X_fooling, perturbation, y, y_pred, y_attack_pred)
-
-        # if idx == 3:
-        #     x, y = batch
-        #     x = x[0].unsqueeze(0)
-        #     y = y[0].unsqueeze(0)
-        #     y_pred = model.predict(x)
-        #     y_target = hide_one_label_nearest(y, 1)
-
-            # X_fooling, perturbation = attack.make_perturbation(model, x, y_target, learning_rate=0.01, max_iter=1)
-            # y_attack_pred = model.predict(X_fooling)
-            #
-            # print("before attack", count_label(y_pred))
-            # print("after attack", count_label(y_attack_pred))
-            #
-            # visualize_attack_results("image_gd", x, X_fooling, perturbation, y, y_pred, y_attack_pred)
 
 
 if __name__ == "__main__":
